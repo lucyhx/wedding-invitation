5a156e9c72e8c9da1166e9ac9db048b516ab288c
lucyhx <lucyhx@users.noreply.github.com>
2017-04-07 17:01:11 +0800

    add full_page from AlloyTeam/AlloyTouch

---
+++ js/index.js
@@ -0,0 +1,51 @@
+var target = document.querySelector('#demo-img')
+//给element注入transform属性
+Transform(target, true)
+
+new AlloyTouch({
+  touch: '#demo1',//反馈触摸的dom
+  vertical: false,//不必需，默认是true代表监听竖直方向touch
+  target: target, //运动的对象
+  property: 'rotateY',  //被滚动的属性
+  touchStart: function (evt) {
+    evt.stopPropagation()
+  },
+  bindSelf: true,
+  touchMove: function (evt) {
+    evt.stopPropagation()
+  }
+
+})
+
+var scroller = document.querySelector('#scroller')
+Transform(scroller, true)
+
+var at = new AlloyTouch({
+  touch: '#demo0',//反馈触摸的dom
+  target: scroller, //运动的对象
+  property: 'translateY',  //被滚动的属性
+  min: 250 - 2000,
+  touchStart: function (evt) {
+    evt.stopPropagation()
+  },
+  touchMove: function (evt) {
+    evt.stopPropagation()
+  },
+  bindSelf: true,
+  max: 0 //不必需,滚动属性的最大值
+})
+
+var pb
+var fp = new AlloyTouch.FullPage('#fullpage', {
+  animationEnd: function () {
+
+  },
+  leavePage: function (index) {
+    console.log('leave' + index)
+  },
+  beginToPage: function (index) {
+    console.log('to' + index)
+    pb.to(index / (this.length - 1))
+  }
+})
+pb = new ProgressBar('#progress', fp.length - 1)
