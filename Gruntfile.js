c5e9812e12296ed145ab6036db3bdeaa3bb03b57
lucyhx <lucyhx@users.noreply.github.com>
2017-04-13 01:34:54 +0800

    version v1.0.0

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
+        stripBanners: true
+      },
+      dist: {
+        src: [
+          'js/transform.js',
+          'js/alloy_touch.js',
+          'js/alloy_touch.full_page.js',
+          'js/progress_bar.js',
+          'js/index.js',
+          'js/snow.js',
+        ],
+        dest: 'dist/js/index.js'
+      }
+    },
+    uglify: {
+      options: {},
+      dist: {
+        files: {
+          'dist/js/index.min.js': 'dist/js/index.js'
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        keepSpecialComments: 0
+      },
+      compress: {
+        files: {
+          'dist/css/index.css': [
+            'css/animate.min.css',
+            'css/index.css',
+          ]
+        }
+      }
+    },
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'dist/index.html': 'index.html'      // 'destination': 'source'
+        }
+      },
+      dev: {                                       // Another target
+        files: {
+          'dist/index.html': 'index.html'
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-htmlmin')
+
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'htmlmin'])
+}
--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 var target = document.querySelector('#page-1-footer')
-//给element注入transform属性
 Transform(target, true)
 
 new AlloyTouch({
-  touch: '#page-1-footer',//反馈触摸的dom
-  vertical: false,//不必需，默认是true代表监听竖直方向touch
-  target: target, //运动的对象
-  property: 'rotateY',  //被滚动的属性
+  touch: '#page-1-footer',
+  vertical: false,
+  target: target,
+  property: 'rotateY',
   touchStart: function (evt) {
     evt.stopPropagation()
   },
@@ -20,14 +19,26 @@ new AlloyTouch({
 var pb
 var fp = new AlloyTouch.FullPage('#fullpage', {
   animationEnd: function () {
-
   },
   leavePage: function (index) {
-    console.log('leave' + index)
   },
   beginToPage: function (index) {
-    console.log('to' + index)
     pb.to(index / (this.length - 1))
   }
 })
 pb = new ProgressBar('#progress', fp.length - 1)
+
+function showInvitation () {
+  document.getElementById('invitation-text-1').innerHTML =
+    '邀请您参加我们的婚礼'
+  document.getElementById('invitation-text-2').innerHTML =
+    '在这个重要的时刻<br>有您的参与我们倍感幸福'
+  document.getElementById('invitation-text-3').innerHTML =
+    '20xx年xx月xx日<br>周六 晚上18点<br>XX市XXX<br>XXX<br>XXX'
+}
+
+showInvitation()
+
+document.addEventListener('WeixinJSBridgeReady', function () {
+  document.getElementById('audio-music').play()
+}, false)
