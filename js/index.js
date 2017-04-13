f607480ee74b7dd2e8b178c083ce7ff05e722c7a
lucyhx <lucyhx@users.noreply.github.com>
2017-04-13 16:26:16 +0800

    move js words to html

--- js/index.js
+++ js/index.js
@@ -28,17 +28,6 @@ var fp = new AlloyTouch.FullPage('#fullpage', {
 })
 pb = new ProgressBar('#progress', fp.length - 1)
 
-function showInvitation () {
-  document.getElementById('invitation-text-1').innerHTML =
-    '邀请您参加我们的婚礼'
-  document.getElementById('invitation-text-2').innerHTML =
-    '在这个重要的时刻<br>有您的参与我们倍感幸福'
-  document.getElementById('invitation-text-3').innerHTML =
-    '20xx年xx月xx日<br>周六 晚上18点<br>XX市XXX<br>XXX<br>XXX'
-}
-
-showInvitation()
-
 document.addEventListener('WeixinJSBridgeReady', function () {
   document.getElementById('audio-music').play()
 }, false)
