572636c104974a27bccefb5eab4f46cd32fe7dcc
lucyhx <lucyhx@users.noreply.github.com>
2017-04-14 17:29:40 +0800

    init project

---
+++ css/README.md
@@ -0,0 +1,6 @@
+# CSS
+
+## animate
+- [GitHub](https://github.com/daneden/animate.css)
+- [Website](https://daneden.github.io/animate.css/)
+- [CDN](https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css)
---
+++ js/index.js
@@ -0,0 +1,71 @@
+var target = document.querySelector('#page-1-footer')
+Transform(target, true)
+
+new AlloyTouch({
+  touch: '#page-1-footer',
+  vertical: false,
+  target: target,
+  property: 'rotateY',
+  touchStart: function (evt) {
+    evt.stopPropagation()
+  },
+  bindSelf: true,
+  touchMove: function (evt) {
+    evt.stopPropagation()
+  }
+
+})
+
+var pb
+var fp = new AlloyTouch.FullPage('#fullpage', {
+  animationEnd: function () {
+  },
+  leavePage: function (index) {
+  },
+  beginToPage: function (index) {
+    pb.to(index / (this.length - 1))
+  }
+})
+pb = new ProgressBar('#progress', fp.length - 1)
+
+document.addEventListener('WeixinJSBridgeReady', function () {
+  document.getElementById('audio-music').play()
+}, false)
+
+function insertCSS (selector, rule) {
+  if (document.styleSheets && document.styleSheets.length) {
+    try {
+      addCSSRule(document.styleSheets[0], selector, rule, 288)
+    } catch (ex) {
+      console.error(ex.message, rule)
+    }
+    function addCSSRule (sheet, selector, rules, index) {
+      if ('insertRule' in sheet) {
+        sheet.insertRule(selector + '{' + rules + '}', index)
+      } else if ('addRule' in sheet) {
+        sheet.addRule(selector, rules, index)
+      }
+    }
+  } else {
+    var style = document.createElement('style')
+    style.innerHTML = rule
+    document.head.appendChild(style)
+  }
+  return
+}
+
+if (window.innerHeight > 0 && window.innerWidth / window.innerHeight > 1.5 / 2) {
+  try {
+    insertCSS('#full-page', 'width: '
+      + (100 * 1.5 * window.innerHeight) / (2 * window.innerWidth)
+      + '%;')
+  } catch (e) {
+    console.error(e)
+  }
+  try {
+    insertCSS('.invitations h1', 'line-height: 36px; font-size: 1.2em;')
+    insertCSS('.invitations p', 'line-height: 27px; font-size: 0.9em;')
+  } catch (e) {
+    console.error(e)
+  }
+}
